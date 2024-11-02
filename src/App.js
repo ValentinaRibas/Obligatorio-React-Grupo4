2724f092dd0d8b6a1417b0c0c9e83551a40c7b19
veroeb <vecheza@gmail.com>
2024-11-02 17:17:08 -0300

    Listo componente aparte Sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Profile from './components/Profile';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Profile from "./components/Profile";
+// import Home from "./components/Home";
+// import Notifications from "./components/Notifications";
+// import Create from "./components/Create";
 
 function App() {
   return (
-    <div className="App">
-      <Profile />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/create" element={<Create />} /> */}
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Profile.css";
+import Sidebar from "../components/Sidebar";
 import profilePic from "../resources/images/Harrypotter.webp";
 
 const Profile = () => {
@@ -21,34 +22,69 @@ const Profile = () => {
       "https://cdn.britannica.com/81/152981-050-7891A7CF/Daniel-Radcliffe-Harry-Potter-and-the-Philosophers.jpg",
       "https://en.vogue.me/wp-content/uploads/2024/06/promo-harry-potter.jpg",
       "https://assets.vogue.in/photos/5f23c04f1d33754d11eaf778/2:3/w_2560,c_limit/harry-potter-philosophers-stone-portrait-8.jpg",
-      "https://images.ctfassets.net/usf1vwtuqyxm/3SQ3X2km8wkQIsQWa02yOY/8801d7055a3e99dae8e60f54bb4b1db8/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg?w=914&q=70&fm=jpg"
+      "https://images.ctfassets.net/usf1vwtuqyxm/3SQ3X2km8wkQIsQWa02yOY/8801d7055a3e99dae8e60f54bb4b1db8/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg?w=914&q=70&fm=jpg",
     ],
+  };
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [username, setUsername] = useState(user.username);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSaveClick = () => {
+    setIsEditing(false);
+  };
+
+  const handleImageClick = (index) => {
+    alert(`Imagen ${index + 1} clickeada!`);
   };
 
   return (
     <div className="profile-page">
-      <aside className="sidebar">
-        <h1>fakestagram</h1>
-        <ul>
-          <li>Home</li>
-          <li>Notifications</li>
-          <li>Create</li>
-          <li>Profile</li>
-        </ul>
-      </aside>
+      <Sidebar />
 
       <main className="profile-main">
         <div className="profile-header">
-          <img src={user.profileImage} alt="Profile" className="profile-image" />
+          <img
+            src={user.profileImage}
+            alt="Profile"
+            className="profile-image"
+          />
           <div className="profile-details">
             <div className="username-edit-container">
-              <h2>{user.username}</h2>
-              <button className="edit-button">Edit profile</button>
+              {isEditing ? (
+                <input
+                  type="text"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  className="username-input"
+                />
+              ) : (
+                <h2>{username}</h2>
+              )}
+              <button
+                className="edit-button"
+                onClick={isEditing ? handleSaveClick : handleEditClick}
+              >
+                {isEditing ? "Save" : "Edit profile"}
+              </button>
             </div>
             <div className="profile-stats">
-              <p><strong>{user.posts}</strong> posts</p>
-              <p><strong>{user.followers}</strong> followers</p>
-              <p><strong>{user.following}</strong> following</p>
+              <p>
+                <strong>{user.posts}</strong> posts
+              </p>
+              <p>
+                <strong>{user.followers}</strong> followers
+              </p>
+              <p>
+                <strong>{user.following}</strong> following
+              </p>
             </div>
             <p className="profile-bio">{user.bio}</p>
           </div>
@@ -56,7 +92,17 @@ const Profile = () => {
 
         <div className="profile-photos">
           {user.photos.map((photo, index) => (
-            <img key={index} src={photo} alt={`User post ${index}`} className="profile-photo" />
+            <button
+              key={index}
+              className="photo-button"
+              onClick={() => handleImageClick(index)}
+            >
+              <img
+                src={photo}
+                alt={`User post ${index}`}
+                className="profile-photo"
+              />
+            </button>
           ))}
         </div>
       </main>
