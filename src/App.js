1063b880699ccec04c90b9fbe2727018d1c94e72
ValentinaRibas <ribasrvale@gmail.com>
2024-11-06 18:41:51 -0300

    Fixes for post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Profile from "./components/Profile";
-// import Home from "./components/Home";
-// import Notifications from "./components/Notifications";
-// import Create from "./components/Create";
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/create" element={<Create />} /> */}
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </Router>
+    <div data-theme='light' className="App">
+    </div>
   );
 }
 
