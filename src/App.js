bdba31574d7b1d0f043e17191ec67dc378dbc749
veroeb <vecheza@gmail.com>
2024-11-07 00:15:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import Profile from "./components/Profile";
 import Login from "./components/Auth/Login/Login";
 import Register from "./components/Auth/Register/Register";
 
 function App() {
+  const isAuthenticated = localStorage.getItem("token");
+
   return (
     <Router>
       <div className="App" data-theme="light">
         <Routes>
-          <Route path="/profile/:userId" element={<Profile />} />
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route
+            path="/profile/:userId"
+            element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
+          />
         </Routes>
       </div>
     </Router>
