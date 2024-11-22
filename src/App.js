821f40dc11bc6dad3f140f56e1aa5666b57d0d1a
veroeb <vecheza@gmail.com>
2024-11-22 01:06:13 -0300

    Solucionado ir al feed luego de loguearse

--- src/App.js
+++ src/App.js
@@ -9,20 +9,38 @@ import "./App.css";
 import Profile from "./components/Profile";
 import Login from "./components/Auth/Login/Login";
 import Register from "./components/Auth/Register/Register";
-import CreatePost from "./components/CreatePost"
-import Feed from "./components/Feed"
+import CreatePost from "./components/CreatePost";
+import Feed from "./components/Feed";
 
 function App() {
-  const isAuthenticated = localStorage.getItem("token");
+  const isAuthenticated = !!localStorage.getItem("token");
 
   return (
     <Router>
       <div className="App" data-theme="light">
         <Routes>
-          <Route path="/" element={<Feed />} />
+          <Route
+            path="/"
+            element={
+              isAuthenticated ? (
+                <Navigate to="/feed" />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/create" element={<CreatePost />} />
+          <Route
+            path="/feed"
+            element={isAuthenticated ? <Feed /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/create"
+            element={
+              isAuthenticated ? <CreatePost /> : <Navigate to="/login" />
+            }
+          />
           <Route
             path="/profile/:userId"
             element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
