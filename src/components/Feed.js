5932c98df1dbcb09f953610a6c43abc2ad43331c
veroeb <vecheza@gmail.com>
2024-11-22 00:32:47 -0300

    Fixes en el post en su propio perfil o en el perfil de la otra persona

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/Feed.css';  
-import FeedHeader from './FeedHeader';
-import FeedFooter from './FeedFooter';
-import Post from './Post';
+import React, { useState, useEffect } from "react";
+import "../styles/Feed.css";
+import Post from "./Post";
 import Sidebar from "../components/Sidebar";
 
 const Feed = () => {
   const [posts, setPosts] = useState([]);
   const token = localStorage.getItem("token");
   const apiUrl = "http://localhost:3001";
+
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const currentUserId = storedUser?._id;
 
   const getPosts = async () => {
     const response = await fetch(`${apiUrl}/api/posts/feed`, {
@@ -40,8 +41,9 @@ const Feed = () => {
               profileImage={post.user.profilePicture}
               username={post.user.username}
               userId={post.user._id}
+              currentUserId={currentUserId}
               time={post.createdAt}
-              image={apiUrl + '/' + post.imageUrl}
+              image={apiUrl + "/" + post.imageUrl}
               caption={post.caption}
               likes={post.likes.length}
               comments={post.comments.length}
